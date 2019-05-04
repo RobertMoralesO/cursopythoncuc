bd88c946d28c94e5a31d487bc5d651279f8cb16f
Roberto Morales <roberto@morales.com.co>
2019-05-04 09:21:30 -0500

    Ejercicio condicional

---
+++ MayorEdad.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  4 09:07:46 2019
+
+@author: android
+"""
+print("Digite la edad")
+edad = input()
+edad = int(edad)
+"""if edad >= 18:
+    print('Es Mayor de Edad')
+else:
+    print('Es Menor de Edad')
+
+   """ 
+# Si de linea con sino
+print('Es Mayor de edad') if edad >= 18 else print('Menor de Edad')
+
+# si de linea sin sino
+if edad >= 18: print("Es Mayor de edad")
+    
+    
+    
+    
